901f8dfdc0354aea21ca8681479c669caec98f43
Daniel <daniel.mohns@posteo.de>
2023-10-11 15:23:03 +0200

    Using `long_description` enables description on PyPI

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup, find_packages
 
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="openpaygo", 
@@ -8,6 +12,8 @@ setup(
     license='MIT',
     author="Solaris Offgrid",
     url='https://github.com/EnAccess/OpenPAYGO-python/',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     install_requires=[
         'siphash',
     ],
