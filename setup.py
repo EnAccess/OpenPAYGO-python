7e158334f675fbdb97032b1b32699a55e6397405
Daniel <daniel.mohns@posteo.de>
2023-10-12 14:12:44 +0200

    Use `setup.cfg` over `setup.py`

--- setup.py
+++ setup.py
@@ -1,20 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-# read the contents of your README file
-from pathlib import Path
-this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
-
-setup(
-    name="openpaygo", 
-    packages=find_packages(),
-    version='0.5.2',
-    license='MIT',
-    author="Solaris Offgrid",
-    url='https://github.com/EnAccess/OpenPAYGO-python/',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    install_requires=[
-        'siphash',
-    ],
-)
+setup()
